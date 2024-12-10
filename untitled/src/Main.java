1c74b8fb89de1e36205eb4c232638e7b785a3ec6
stouf <ouadad.mustapha01@gmail.com>
2024-12-10 16:18:59 +0100

    fonctions séparées pour chaque opération:factorielle/et ajouter le menu de calculatrice

--- untitled/src/Main.java
+++ untitled/src/Main.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Main {
     public static double addition(double a,double b){
         return a+b;
@@ -22,10 +24,40 @@ public class Main {
         if (a<0){
             System.out.println("Erreur: Racine carrée d'un nombre négatif !");
             return -1;
+        }else {
+            return Math.sqrt(a);
         }
     }
+    public static long factorielle(int n){
+        if (n < 0) {
+            System.out.println("Erreur: Factorielle d'un nombre négatif !");
+            return -1;
+        }
+        long fact=1;
+        for (int i=1;i<=n;i++){
+            fact *=i;
+        }
+      return fact;
+    }
     public static void main(String[] args) {
+        Scanner scanner =new Scanner(System.in);
+        while (true){
+            System.out.println("----------------Menu Calculatrice----------------");
+            System.out.println("1. Addition");
+            System.out.println("2. Soustraction");
+            System.out.println("3. Multiplication");
+            System.out.println("4. Division");
+            System.out.println("5. Puissance");
+            System.out.println("6. Racine carrée");
+            System.out.println("7. Factorielle");
+            System.out.println("0. Quitter");
+            System.out.print("Choisissez une option: ");
+            int choix = scanner.nextInt();
+            if (choix==0){
+                System.out.println("Programme terminé.");
+            }
 
+        }
 
     }
 }
